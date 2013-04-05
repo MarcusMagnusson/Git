17bf2bf733fd8b03327d3e0189b52d966f098573
Marcus Magnusson <MJEM@Marcuss-MacBook-Pro.local>
2013-04-05 10:39:45 +0200

    add

---
+++ Git.h
@@ -0,0 +1,13 @@
+//
+//  Git.h
+//  Git
+//
+//  Created by Marcus Magnusson on 2013-04-05.
+//  Copyright (c) 2013 Marcus Magnusson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Git : NSObject
+
+@end
---
+++ Git.m
@@ -0,0 +1,13 @@
+//
+//  Git.m
+//  Git
+//
+//  Created by Marcus Magnusson on 2013-04-05.
+//  Copyright (c) 2013 Marcus Magnusson. All rights reserved.
+//
+
+#import "Git.h"
+
+@implementation Git
+
+@end
