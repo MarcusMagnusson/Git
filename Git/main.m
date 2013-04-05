0e8af06a3f16dec928a9bfb77a8cc26dedafae5e
Marcus Magnusson <MJEM@Marcuss-MacBook-Pro.local>
2013-04-05 10:30:51 +0200

    Initial Commit

---
+++ Git/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Git
+//
+//  Created by Marcus Magnusson on 2013-04-05.
+//  Copyright (c) 2013 Marcus Magnusson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
